d3bdc1b23670171fe89a36fb065bca93ddcb35f1
kmsingh <kmsingh0999@gmail.com>
2024-12-25 21:34:24 +0530

    updated

--- template.py
+++ template.py
@@ -5,14 +5,15 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
 
-list_of_files=[
+list_of_files = [
     "src/__init__.py",
     "src/helper.py",
-    "src/prompt.py"
+    "src/prompt.py",
     ".env",
     "setup.py",
     "app.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+   " test.py"
 ]
 
 for filepath in list_of_files:

316582ee1571dfd1ffefd958dd382065102eb690
kmsingh <kmsingh0999@gmail.com>
2024-12-25 16:02:01 +0530

    update env file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="KM SINGH",
+    author_email="kmsingh0999@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
